15575951ae5c6f4f99a593491113e9dd1df650fb
Ogunsola <ogunsoladebayo@gmail.com>
2020-06-02 23:11:50 +0100

    folder structure set-up

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
 
 
-    
+    // scan scripts directory for files
+    $filesArray = scandir('scripts/');
+
+    $testCheck = Array(
+
+    );
+
+    foreach ($filesArray as $file) {
+        $fileSplit = explode('.', $file);
+        $fileExtension = $fileSplit[1];
+        if ($fileExtension == 'py'){
+            // call function
+        }elseif($fileExtension == 'js'){
+            // call function
+        }elseif($fileExtension == 'cs'){
+            // call function
+        }else{
+            // file extension not compatible...
+        }
+    }
