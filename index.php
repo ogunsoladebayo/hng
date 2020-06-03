d2398c562f61334857c43918b64a2eba850ea670
Ogunsola <ogunsoladebayo@gmail.com>
2020-06-03 02:54:48 +0100

    added string check

--- index.php
+++ index.php
@@ -11,16 +11,22 @@
         $output = exec($fileExtension. ' '. $file);
         return $output;
     }
-
-    // function checkMatch($output){
-        
-    // }
+    function checkMatch($output){
+        $searchTerm1 = "Hello World, this is";
+        $searchTerm2 = "for stage 2 task";    
+        if(strpos($output, $searchTerm1) !== false && strpos($output, $searchTerm2) !== false){
+            $check = 'Pass';
+        }else{
+            $check = 'Fail';
+        }
+        return $check;
+    }
 
 
     foreach ($filesArray as $file) {
         $fileSplit = explode('.', $file);
         $fileExtension = $fileSplit[1];
-        if ($fileExtension == 'py'){
+        if($fileExtension == 'py'){
             // call function
             $output = runScript('python', $file);
         }elseif($fileExtension == 'js'){
@@ -31,7 +37,9 @@
             $output = runScript('php', $file);
         }else{
             // file extension not compatible...
+            $check = 'not compatible';
         }
-        $allEntry .= ($output. '<br>');
+        $check = checkMatch($output);
+        $result .= ($check. '<br>');
     }
-    echo $allEntry;
+    echo $result;
